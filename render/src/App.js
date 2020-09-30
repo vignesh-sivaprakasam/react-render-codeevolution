db44d18703e44191e94ece6a8f005c4c535ff394
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-09-30 21:54:04 +0530

    Use State render

---
+++ render/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UseState from './components/usestate/UseState';
+
+function App() {
+  return (
+    <div className="App">
+      <UseState />
+    </div>
+  );
+}
+
+export default App;
